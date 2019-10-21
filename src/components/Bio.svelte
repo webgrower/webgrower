8d270f5770c62255e45acf578247e7af3dc7f897
Nikita Makhov <n.mahov@corp.mail.ru>
2019-10-21 12:39:40 +0300

    Initial Webgrower commit

---
+++ src/components/Bio.svelte
@@ -0,0 +1,29 @@
+<style>
+  div {
+    align-items: center;
+    display: flex;
+  }
+
+  img {
+    width: 100px;
+    height: 100px;
+    border-radius: 100px;
+    /* border: 3px solid #fd6378; */
+    display: block;
+    margin-right: 20px;
+  }
+
+  p {
+    font-size: 1.125rem;
+  }
+</style>
+
+<div>
+  <img src="ron-swanson-simpsons.jpg" alt="Nikita Makhov">
+  <p>
+    Hi, I'm <strong>Defite</strong>. I'm a frontend web-developer from Moscow, Russia 🇷🇺.
+    You can follow me on <a href="https://www.twitter.com/life_maniac" target="_blank">Twitter</a>,
+    see some of my work on <a href="https://www.github.com/Defite" target="_blank">GitHub</a>,
+    or read more about me on <a href="https://defite.rum" target="_blank">my website</a>.
+  </p>
+</div>
